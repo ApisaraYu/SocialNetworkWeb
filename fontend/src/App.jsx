fbf676e70d3259e72a8c24b5d40ec07a9b3f4633
ApisaraYu <apisara.yusomboon@gmail.com>
2026-03-07 17:59:35 +0700

    add comment

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <body class="bg-slate-100">
-      <div class="bg-white fixed top-0 left-0 right-0 z-50 shadow-sm">
+    <body class="bg-slate-100"> {/* เปลี่ยนสี bg เป็น slate-100 */}
+      <div class="bg-white fixed top-0 left-0 right-0 z-50 shadow-sm"> {/* สร้างแทบ nav bar ขึ้นมา */}
         <div class="text-lg font-extrabold font-sans text-amber-500"><span class="text-cyan-700">Socialio</span>
 
         </div>
