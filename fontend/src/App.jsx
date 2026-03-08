c40707aefbf8d7679f2891000b0996ac5841e20d
ApisaraYu <apisara.yusomboon@gmail.com>
2026-03-08 19:37:44 +0700

    update main page
    add website icon, update nav bar

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import logo from './assets/Socialio_logo.png'
 
 function App() {
   return (
     <body class="bg-slate-100"> {/* เปลี่ยนสี bg เป็น slate-100 */}
       <div class="bg-white fixed top-0 left-0 right-0 z-50 shadow-sm"> {/* สร้างแทบ nav bar ขึ้นมา */}
-        <div class="text-lg font-extrabold font-sans text-amber-500"><span class="text-cyan-700">Socialio</span>
-
+        <div class="text-3xl font-extrabold font-sans text-cyan-700 flex items-center gap-2">
+          <img class="object-cover h-20" src={logo}/>Socialio
         </div>
       </div>
     </body>
